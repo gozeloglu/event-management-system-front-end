752530ab7a996cd19782bb0b9de6bd80fd1cca78
gozeloglu <gozeloglu@gmail.com>
2020-07-22 21:46:09 +0300

    new table is added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import './App.css';
 import Title from "./Components/Title";
 import Button from "@material-ui/core/Button";
 import PlusIcon from '@material-ui/icons/Add';
-
-const eventDetail = "We are back! this is our first event after Corona Virus. We are waiting for you!"
-const startDate = "17.07.2020"
-const endDate = "20.07.2020"
-const startTime = "20.00"
-const endTime = "10.00"
-const registrationDeadline = "16.07.2020 - 23.59"
-
+import MaterialTableDemo from "./Components/MeetupList";
 
 class App extends Component {
 
@@ -18,27 +11,21 @@ class App extends Component {
         return (
             <div className="App">
                 <Title/>
-                <Button variant={"contained"}
-                        color={"primary"}
-                        style={{float: "right"}}
-                        onClick={() => {}}
-                        startIcon={<PlusIcon/>}
-                >
-                    Add New Meetup</Button>
+                <div>
+                    <Button variant={"contained"}
+                            color={"primary"}
+                            style={{float: "right"}}
+                            onClick={() => {
+                            }}
+                            startIcon={<PlusIcon/>}
+                    >
+                        Add New Meetup</Button>
+                </div>
+                <MaterialTableDemo/>
+
             </div>
         );
     }
-
-    deleteEvent = (id) => {
-        this.setState({
-            events: this.state.events.filter(event => event.id !== id)
-        })
-    }
-}
-
-Event.defaulProps = {
-    title: "Event Manager System",
-    names: ["Gökhan", "Ahmet", "Ali"]
 }
 
 export default App;
---
+++ src/Components/TableIcons.js
@@ -0,0 +1,32 @@
+import React, { forwardRef } from "react";
+import {
+    AddBox,
+    ArrowUpward,
+    Check,
+    ChevronLeft,
+    ChevronRight,
+    Clear,
+    DeleteOutline,
+    Edit,
+    FirstPage,
+    LastPage,
+    Search,
+} from "@material-ui/icons";
+
+export default {
+    // tableIcons
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+        <ChevronLeft {...props} ref={ref} />
+    )),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+};
