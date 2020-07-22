9eb3cc0c85786f37788735520619469d68bfe54a
gozeloglu <gozeloglu@gmail.com>
2020-07-22 17:21:43 +0300

    some components are deleted material-ui button is added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Title from "./Components/Title";
-import Event from './Components/Event';
+import Button from "@material-ui/core/Button";
+import PlusIcon from '@material-ui/icons/Add';
 
 const eventDetail = "We are back! this is our first event after Corona Virus. We are waiting for you!"
 const startDate = "17.07.2020"
@@ -14,67 +14,25 @@ const registrationDeadline = "16.07.2020 - 23.59"
 
 class App extends Component {
 
-    state = {
-        events: [{
-            id: 1,
-            eventName: "Flutter Day",
-            address: "Hacettepe University CS Building Ankara/Turkey",
-            eventDetails: "We are back! this is our first event after Corona Virus. We are waiting for you!",
-            startDate: "17.07.2020",
-            endDate: "20.07.2020",
-            startTime: "20.00",
-            endTime: "10.00",
-            registrationDeadline: "16.07.2020 - 23.59"
-        }, {
-            id: 2,
-            eventName: "React JS Day",
-            address: "METU CS Building Ankara/Turkey",
-            eventDetails: "We are back! this is our first event after Corona Virus. We are waiting for you!",
-            startDate: "27.07.2021",
-            endDate: "30.07.2021",
-            startTime: "20.00",
-            endTime: "10.00",
-            registrationDeadline: "26.07.2021 - 23.59"
-        },{
-            id: 3,
-            eventName: "React JS Day",
-            address: "Sabanci CS Building Ankara/Turkey",
-            eventDetails: "We are back! this is our first event after Corona Virus. We are waiting for you!",
-            startDate: "27.07.2021",
-            endDate: "30.07.2021",
-            startTime: "20.00",
-            endTime: "10.00",
-            registrationDeadline: "26.07.2021 - 23.59"
-        }, {
-            id: 4,
-            eventName: "React JS Day",
-            address: "Bilkent CS Building Ankara/Turkey",
-            eventDetails: "We are back! this is our first event after Corona Virus. We are waiting for you!",
-            startDate: "27.07.2021",
-            endDate: "30.07.2021",
-            startTime: "20.00",
-            endTime: "10.00",
-            registrationDeadline: "26.07.2021 - 23.59"
-        }, {
-            id: 5,
-            eventName: "React JS Day",
-            address: "ITU CS Building Ankara/Turkey",
-            eventDetails: "We are back! this is our first event after Corona Virus. We are waiting for you!",
-            startDate: "27.07.2021",
-            endDate: "30.07.2021",
-            startTime: "20.00",
-            endTime: "10.00",
-            registrationDeadline: "26.07.2021 - 23.59"
-        }]
-    }
-
     render() {
         return (
             <div className="App">
                 <Title/>
-                <Event events = {this.state.events} />
+                <Button variant={"contained"}
+                        color={"primary"}
+                        style={{float: "right"}}
+                        onClick={() => {}}
+                        startIcon={<PlusIcon/>}
+                >
+                    Add New Meetup</Button>
             </div>
         );
+    }
+
+    deleteEvent = (id) => {
+        this.setState({
+            events: this.state.events.filter(event => event.id !== id)
+        })
     }
 }
 
